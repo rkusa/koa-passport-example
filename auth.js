8deaad835cbe3e1234a5a76b070bd95ea3bb2bdf
Petter <petter@pttr.se>
2017-12-05 23:12:03 +0100

    added missing word

--- auth.js
+++ auth.js
@@ -1,7 +1,7 @@
 const passport = require('koa-passport')
 
 const fetchUser = (() => {
-  // This is an example! Use password hashing in your
+  // This is an example! Use password hashing in your project
   const user = { id: 1, username: 'test', password: 'test' }
   return async function() {
     return user
